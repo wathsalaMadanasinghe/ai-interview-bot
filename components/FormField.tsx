a21f235a9ed61108fa2e68b269c8d645af798197
wathsalaMadanasinghe <cwathsala123@gmail.com>
2025-04-29 08:32:18 +0530

    sign-in and sign-up
    Initial commit with sign-in and sign-up functionality

---
+++ components/FormField.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Control, Controller, FieldValues, Path } from "react-hook-form";
+import {
+  FormControl,
+  FormDescription,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+
+interface FormFieldProps<T extends FieldValues> {
+  control: Control<T>;
+  name: Path<T>;
+  label: string;
+  placeholder?: string;
+  type?: "text" | "email" | "password" | "file";
+}
+
+const FormField = <T extends FieldValues>({
+  control,
+  name,
+  label,
+  placeholder,
+  type = "text",
+}: FormFieldProps<T>) => (
+  <Controller
+    name={name}
+    control={control}
+    render={({ field }) => (
+      <FormItem>
+        <FormLabel className="label">{label}</FormLabel>
+        <FormControl>
+          <Input className="input" placeholder={placeholder} {...field} />
+        </FormControl>
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+);
+
+export default FormField;
